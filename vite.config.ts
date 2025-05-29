6fc3e7129632d00c67c551259eea5a91ebf20d67
Hasan Iqbal <swapniliqbal@gmail.com>
2025-05-29 15:17:05 -0400

    chore: update dependencies and configuration for TypeScript and Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        content: resolve(__dirname, 'src/contentScript.tsx'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
